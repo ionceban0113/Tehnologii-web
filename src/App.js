14750689b7ef3976c070aa56b3eb50e52d0e6787
ionceban0113 <ion.ceban0113@gmail.com>
2023-04-06 18:26:14 +0300

    Laboratorul 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h>Ceban Ion CR-201</h>
+    </div>
+  );
+}
+
+export default App;
